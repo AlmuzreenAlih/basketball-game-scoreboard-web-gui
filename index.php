9a8964ebb50738eb8026f4a1bb92117ff3b53385
Almuzreen Alih <almuzreen.11@gmail.com>
2023-09-19 16:46:13 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Masjid Harmony Live Scoreboard</title>
+    <link href="https://fonts.cdnfonts.com/css/digital-7-mono" rel="stylesheet">
+                
+</head>
+<body>
+    
+</body>
+</html>
