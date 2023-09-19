a1d53789bf590e3c635c97e162a1c837765b7a22
Almuzreen Alih <almuzreen.11@gmail.com>
2023-09-19 18:18:13 +0800

    create scss and use flexbox as general layout

--- index.php
+++ index.php
@@ -5,9 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Masjid Harmony Live Scoreboard</title>
     <link href="https://fonts.cdnfonts.com/css/digital-7-mono" rel="stylesheet">
-                
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    
+    <section>
+        <div class="card">
+
+        </div>
+    </section>
+    <section>
+        <div class="card"></div>
+        <div class="card"></div>
+    </section>
 </body>
 </html>
