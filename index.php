2c49d59875a66b1f337f45d429d59496473ce912
Almuzreen Alih <almuzreen.11@gmail.com>
2023-09-21 17:53:48 +0800

    create minus buttons for team1 section

--- index.php
+++ index.php
@@ -5,19 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Masjid Harmony Live Scoreboard</title>
     <link href="https://fonts.cdnfonts.com/css/digital-7-mono" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.css?x=<?php echo strval(rand())?>">
 </head>
 <body>
     <section class="panel">
-        <div class="row1">
-            <section class="team1-section">
-                <span>HOME</span>
-                <span>87
-                    <button>+3</button>
-                    <button>+2</button>
-                    <button>+1</button>
+        <div id="row1">
+            <section id="team1-section">
+                <span id="team1-name">HOME</span>
+                <span id="team1-score">87
+                    <button id="team1-button1">+3</button>
+                    <button id="team1-button2">+2</button>
+                    <button id="team1-button3">+1</button>
+                    <button id="team1-button4">-3</button>
+                    <button id="team1-button5">-2</button>
+                    <button id="team1-button6">-1</button>
                 </span>
-                <span>BONUS</span>
+                <span id="team1-bonus">BONUS</span>
             </section>
         </div>
         <div class="row2">b</div>
