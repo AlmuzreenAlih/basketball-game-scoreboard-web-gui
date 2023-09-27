0e4ca3572e9d6929f2cd9d259d42213f1c5d4010
Almuzreen Alih <almuzreen.11@gmail.com>
2023-09-27 18:04:42 +0800

    create basic period click and button click functions

--- functions.js
+++ functions.js
@@ -1,11 +1,76 @@
+var storedData;
+var retrievedGameData;
+var defaultGameData;
+
+function calculateRemainingTime(allottedTime, startTime) {
+    // Calculate the current time
+    const currentTime = new Date();
+  
+    // Calculate the elapsed time in seconds
+    const elapsedSeconds = (currentTime - startTime) / 1000;
+  
+    // Calculate the remaining time in seconds
+    const remainingSeconds = Math.max(allottedTime - elapsedSeconds, 0);
+  
+    // Check if remainingSeconds is less than 60
+    if (remainingSeconds < 60) {
+      // Format the remaining time with seconds as decimals
+      const formattedTime = remainingSeconds.toFixed(1);
+      return formattedTime;
+    } else {
+      // Calculate minutes and seconds
+      const minutes = Math.floor(remainingSeconds / 60);
+      const seconds = Math.floor(remainingSeconds % 60);
+  
+      // Format the remaining time as MM:SS
+      const formattedTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+      return formattedTime;
+    }
+  }  
+
+function DisplayScoreBoardValues() {
+    $("#score1-value").text(retrievedGameData.score1);
+    $("#score2-value").text(retrievedGameData.score2);
+    $("#fouls1-value").text(retrievedGameData.fouls1);
+    $("#fouls2-value").text(retrievedGameData.fouls2); 
+    localStorage.setItem('gameData', JSON.stringify(retrievedGameData));  
+}
+
+function DisplayScoreBoardTimes() {
+    if ((retrievedGameData.periodStarted == 1) || 
+        (retrievedGameData.periodStarted == 1)) {
+        if (typeof(retrievedGameData.periodStartTime)=="string") {
+            retrievedGameData.periodStartTime = new Date(retrievedGameData.periodStartTime);
+        }
+        var displaying = calculateRemainingTime(retrievedGameData.periodTime, retrievedGameData.periodStartTime);
+        $("#period-time-value").text(displaying);
+        retrievedGameData.lastTimeDisplayed = displaying;
+        localStorage.setItem('gameData', JSON.stringify(retrievedGameData));  
+
+      }
+    else if (retrievedGameData.periodStarted == 2) {
+      $("#period-time-value").text(retrievedGameData.lastTimeDisplayed);
+    }
+    else {
+      $("#period-time-value").text("12:00");
+    }
+    $("#shot-clock-value").text(retrievedGameData.shotClockTime);
+    localStorage.setItem('gameData', JSON.stringify(retrievedGameData));
+}
+function myRepeatingFunction() {
+    DisplayScoreBoardTimes();
+    console.log("whut")
+  }
+ 
 $(document).ready(function() {
-    if (localStorage.getItem('gameData')) {
-        var defaultGameData = {
+    if (!localStorage.getItem('gameData')) {
+        defaultGameData = {
           score1: 0,
           score2: 0,
           periodTime: 12*60,   
-          periodStarted: false,
+          periodStarted: 0,
           periodStartTime: new Date(),
+          lastTimeDisplayed: "12:00",
           shotClockTime: 24,
           fouls1: 0,
           fouls2: 0,
@@ -15,27 +80,62 @@ $(document).ready(function() {
         localStorage.setItem('gameData', JSON.stringify(defaultGameData));
       }
 
-      var storedData = localStorage.getItem('gameData');
-      var retrievedGameData = JSON.parse(storedData);
-      $("#score1-value").text("10");
-      $("#score2-value").text("20");
-      $("#fouls1-value").text("30");
-      $("#fouls2-value").text("40");
-      $("#period-time-value").text("10:00");
+      storedData = localStorage.getItem('gameData');
+      retrievedGameData = JSON.parse(storedData);
+      console.log(retrievedGameData);
+      DisplayScoreBoardValues();
+      DisplayScoreBoardTimes();
 
-      $("#score1-value").text(retrievedGameData.score1);
-      $("#score2-value").text(retrievedGameData.score2);
-      $("#fouls1-value").text(retrievedGameData.fouls1);
-      $("#fouls2-value").text(retrievedGameData.fouls2);
-      if (retrievedGameData.periodStarted) {
-        $("#period-time-value").text(retrievedGameData.periodTime);
-      }
-      else {
-        $("#period-time-value").text("12:00");
-      }
-      $("#shot-clock-value").text(retrievedGameData.shotClockTime)
+      const intervalId = setInterval(myRepeatingFunction, 100);
+
 });
 
-function DisplayPeriodTime() {
-    
-}
+$("#team1-button1").click(function() {
+    retrievedGameData.score1 = retrievedGameData.score1 + 3;
+    DisplayScoreBoardValues();
+})
+$("#team1-button2").click(function() {
+    retrievedGameData.score1 = retrievedGameData.score1 + 2;
+    DisplayScoreBoardValues();
+})
+$("#team1-button3").click(function() {
+    retrievedGameData.score1 = retrievedGameData.score1 + 1;
+    DisplayScoreBoardValues();
+})
+$("#team1-button4").click(function() {
+    retrievedGameData.score1 = retrievedGameData.score1 - 3;
+    DisplayScoreBoardValues();
+})
+$("#team1-button5").click(function() {
+    retrievedGameData.score1 = retrievedGameData.score1 - 2;
+    DisplayScoreBoardValues();
+})
+$("#team1-button6").click(function() {
+    retrievedGameData.score1 = retrievedGameData.score1 - 1;
+    DisplayScoreBoardValues();
+})
+$("#period-time-value").click(function() {
+    if (retrievedGameData.periodStarted == 0) {
+      retrievedGameData.periodStartTime = new Date();
+      retrievedGameData.periodStarted = 1;
+      localStorage.setItem('gameData', JSON.stringify(retrievedGameData));  
+    }
+    else if (retrievedGameData.periodStarted == 2) {
+      retrievedGameData.periodStarted = 1;
+      retrievedGameData.resumeTime = new Date();
+            // Assuming retrievedGameData.resumeTime, retrievedGameData.pauseTime, and retrievedGameData.periodStartTime are Date objects
+      const timeDifference = retrievedGameData.resumeTime - retrievedGameData.pauseTime;
+
+      // Calculate the new periodStartTime
+      retrievedGameData.periodStartTime = new Date(retrievedGameData.periodStartTime.getTime() + timeDifference);
+
+      console.log(retrievedGameData.periodStartTime);
+      localStorage.setItem('gameData', JSON.stringify(retrievedGameData));  
+    }
+    else {
+      retrievedGameData.periodStarted = 2;
+      retrievedGameData.pauseTime = new Date();
+      localStorage.setItem('gameData', JSON.stringify(retrievedGameData));  
+    }
+    DisplayScoreBoardTimes();
+})
