8da284bfd2a38c6bfac61f97ef3a84bf6a2971ec
Almuzreen Alih <almuzreen.11@gmail.com>
2024-02-06 20:10:34 +0800

    Add fouls switch possession function

--- functions.js
+++ functions.js
@@ -130,15 +130,15 @@ $(document).ready(function() {
 });
 
 //---------------Button Functions ---------------
-function Team1Button1Function() {retrievedGameData.score1 = retrievedGameData.score1 + 3; DisplayScoreBoardValues();}
-function Team1Button2Function() {retrievedGameData.score1 = retrievedGameData.score1 + 2; DisplayScoreBoardValues();}
+function Team1Button1Function() {SwitchPossesion2(); retrievedGameData.score1 = retrievedGameData.score1 + 3; DisplayScoreBoardValues();}
+function Team1Button2Function() {SwitchPossesion2(); retrievedGameData.score1 = retrievedGameData.score1 + 2; DisplayScoreBoardValues();}
 function Team1Button3Function() {retrievedGameData.score1 = retrievedGameData.score1 + 1; DisplayScoreBoardValues();}
 function Team1Button4Function() {retrievedGameData.score1 = retrievedGameData.score1 - 3; if (retrievedGameData.score1 < 0) {retrievedGameData.score1 = 0;} DisplayScoreBoardValues();}
 function Team1Button5Function() {retrievedGameData.score1 = retrievedGameData.score1 - 2; if (retrievedGameData.score1 < 0) {retrievedGameData.score1 = 0;} DisplayScoreBoardValues();}
 function Team1Button6Function() {retrievedGameData.score1 = retrievedGameData.score1 - 1; if (retrievedGameData.score1 < 0) {retrievedGameData.score1 = 0;} DisplayScoreBoardValues();}
 
-function Team2Button1Function() {retrievedGameData.score2 = retrievedGameData.score2 + 3; DisplayScoreBoardValues();}
-function Team2Button2Function() {retrievedGameData.score2 = retrievedGameData.score2 + 2; DisplayScoreBoardValues();}
+function Team2Button1Function() {SwitchPossesion1(); retrievedGameData.score2 = retrievedGameData.score2 + 3; DisplayScoreBoardValues();}
+function Team2Button2Function() {SwitchPossesion1(); retrievedGameData.score2 = retrievedGameData.score2 + 2; DisplayScoreBoardValues();}
 function Team2Button3Function() {retrievedGameData.score2 = retrievedGameData.score2 + 1; DisplayScoreBoardValues();}
 function Team2Button4Function() {retrievedGameData.score2 = retrievedGameData.score2 - 3; if (retrievedGameData.score2 < 0) {retrievedGameData.score2 = 0;} DisplayScoreBoardValues();}
 function Team2Button5Function() {retrievedGameData.score2 = retrievedGameData.score2 - 2; if (retrievedGameData.score2 < 0) {retrievedGameData.score2 = 0;} DisplayScoreBoardValues();}
@@ -255,6 +255,21 @@ function Reset2() {
 
   localStorage.setItem('gameData', JSON.stringify(retrievedGameData));
 }
+
+function SwitchPossesion1() {
+  if (retrievedGameData.goalSyncPossession) {
+    $("#arrow2").addClass("hidden");
+    $("#arrow1").removeClass("hidden");
+  }
+}
+
+function SwitchPossesion2() {
+  if (retrievedGameData.goalSyncPossession) {
+    $("#arrow1").addClass("hidden");
+    $("#arrow2").removeClass("hidden");
+  }
+}
+
 //---------------Button Function Links---------------
 $("#team1-button1").click(function() {Team1Button1Function();})
 $("#team1-button2").click(function() {Team1Button2Function();})
